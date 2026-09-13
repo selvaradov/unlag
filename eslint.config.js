5526830e33b521c696fe6ba54d9f6ab7f50104fa
selvaradov <33783348+selvaradov@users.noreply.github.com>
2026-09-13 15:38:36 +0100

    eslint: ignore the local netlify folder

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'dev-dist', 'node_modules'] },
+  { ignores: ['dist', 'dev-dist', 'node_modules', '.netlify'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   { files: ['**/*.ts'], rules: { '@typescript-eslint/no-non-null-assertion': 'off' } },
